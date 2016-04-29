53d7a70795ec281cc3ff49e7feee37c223492535
Felix Arntz <felix-arntz@t-online.de>
2016-04-29 19:23:49 +0200

    fixed bug in tests

--- tests/test-taxonomy.php
+++ tests/test-taxonomy.php
@@ -2,14 +2,14 @@
 
 class AT_Tests_Taxonomy extends WP_UnitTestCase {
 	public function test_add_taxonomy() {
-		$status = Attachment_Taxonomies::add_taxonomy( new Attachment_Existing_Taxonomy( 'post_tag' ), true );
+		$status = Attachment_Taxonomies::instance()->add_taxonomy( new Attachment_Existing_Taxonomy( 'post_tag' ), true );
 		$this->assertTrue( $status );
 	}
 
 	public function test_get_taxonomy() {
-		Attachment_Taxonomies::add_taxonomy( new Attachment_Existing_Taxonomy( 'post_tag' ), true );
+		Attachment_Taxonomies::instance()->add_taxonomy( new Attachment_Existing_Taxonomy( 'post_tag' ), true );
 
-		$tax = Attachment_Taxonomies::get_taxonomy( 'post_tag', true );
+		$tax = Attachment_Taxonomies::instance()->get_taxonomy( 'post_tag', true );
 		assertInstanceOf( 'Attachment_Existing_Taxonomy' );
 	}
 }
