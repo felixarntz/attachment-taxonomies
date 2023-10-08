b98a41128a041ebf43e9866419b562f59b76e225
Felix Arntz <flixos90@gmail.com>
2023-10-08 14:45:30 -0500

    Add capability integration tests.

--- tests/phpunit/tests/Attachment_Taxonomy_Capabilities_Tests.php
+++ tests/phpunit/tests/Attachment_Taxonomy_Capabilities_Tests.php
@@ -7,17 +7,25 @@
  */
 
 class Attachment_Taxonomy_Capabilities_Tests extends WP_UnitTestCase {
-	private static $admin_id;
+	private static $user_ids;
 
 	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
-		self::$admin_id = $factory->user->create( array( 'role' => 'administrator' ) );
+		self::$user_ids = array(
+			'admin'       => $factory->user->create( array( 'role' => 'administrator' ) ),
+			'editor'      => $factory->user->create( array( 'role' => 'editor' ) ),
+			'author'      => $factory->user->create( array( 'role' => 'author' ) ),
+			'contributor' => $factory->user->create( array( 'role' => 'contributor' ) ),
+		);
 	}
 
 	public static function wpTearDownAfterClass() {
 		if ( is_multisite() ) {
-			wpmu_delete_user( self::$admin_id );
+			$delete_user = 'wpmu_delete_user';
 		} else {
-			wp_delete_user( self::$admin_id );
+			$delete_user = 'wp_delete_user';
+		}
+		foreach ( self::$user_ids as $user_id ) {
+			call_user_func( $delete_user, $user_id );
 		}
 	}
 
@@ -27,7 +35,7 @@ class Attachment_Taxonomy_Capabilities_Tests extends WP_UnitTestCase {
 	public function test_map_meta_cap( $meta_cap, $expected_caps ) {
 		$instance = new Attachment_Taxonomy_Capabilities();
 
-		$caps = $instance->map_meta_cap( array(), $meta_cap, self::$admin_id );
+		$caps = $instance->map_meta_cap( array(), $meta_cap, self::$user_ids['admin'] );
 		$this->assertSameSets( $expected_caps, $caps );
 	}
 
@@ -45,6 +53,46 @@ class Attachment_Taxonomy_Capabilities_Tests extends WP_UnitTestCase {
 			array( 'delete_attachment_tags', $base_caps ),
 			array( 'assign_attachment_tags', $assign_caps ),
 			array( 'unrelated_cap', array() ),
+		);
+	}
+
+	/**
+	 * @dataProvider data_integration_user_can
+	 */
+	public function test_integration_user_can( $user_role, $cap, $expected ) {
+		if ( $expected ) {
+			$this->assertTrue( user_can( self::$user_ids[ $user_role ], $cap ) );
+		} else {
+			$this->assertFalse( user_can( self::$user_ids[ $user_role ], $cap ) );
+		}
+	}
+
+	public function data_integration_user_can() {
+		return array(
+			array( 'editor', 'manage_attachment_categories', true ),
+			array( 'editor', 'edit_attachment_categories', true ),
+			array( 'editor', 'delete_attachment_categories', true ),
+			array( 'editor', 'assign_attachment_categories', true ),
+			array( 'editor', 'manage_attachment_tags', true ),
+			array( 'editor', 'edit_attachment_tags', true ),
+			array( 'editor', 'delete_attachment_tags', true ),
+			array( 'editor', 'assign_attachment_tags', true ),
+			array( 'author', 'manage_attachment_categories', false ),
+			array( 'author', 'edit_attachment_categories', false ),
+			array( 'author', 'delete_attachment_categories', false ),
+			array( 'author', 'assign_attachment_categories', true ),
+			array( 'author', 'manage_attachment_tags', false ),
+			array( 'author', 'edit_attachment_tags', false ),
+			array( 'author', 'delete_attachment_tags', false ),
+			array( 'author', 'assign_attachment_tags', true ),
+			array( 'contributor', 'manage_attachment_categories', false ),
+			array( 'contributor', 'edit_attachment_categories', false ),
+			array( 'contributor', 'delete_attachment_categories', false ),
+			array( 'contributor', 'assign_attachment_categories', false ),
+			array( 'contributor', 'manage_attachment_tags', false ),
+			array( 'contributor', 'edit_attachment_tags', false ),
+			array( 'contributor', 'delete_attachment_tags', false ),
+			array( 'contributor', 'assign_attachment_tags', false ),
 		);
 	}
 }
