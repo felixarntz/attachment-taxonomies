97e465026110768e94814a6d724c412a3479629b
Felix Arntz <flixos90@gmail.com>
2025-04-13 12:33:28 -0700

    Fix broken build process after dependency update.

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,6 @@
  */
 const config = require( '@wordpress/scripts/config/webpack.config' );
 
-const { sync: glob } = require( 'fast-glob' );
-const { getWebpackEntryPoints } = require( '@wordpress/scripts/utils' );
-
-function getEntryPoints() {
-	const entryPoints = getWebpackEntryPoints();
-
-	const [ entryFile ] = glob(
-		`${ process.env.WP_SRC_DIRECTORY }/index.[jt]s?(x)`,
-		{
-			absolute: true,
-		}
-	);
-	if ( entryFile ) {
-		entryPoints.index = entryFile;
-	}
-
-	return entryPoints;
-}
-
 module.exports = {
 	...config,
-	entry: getEntryPoints,
 };
